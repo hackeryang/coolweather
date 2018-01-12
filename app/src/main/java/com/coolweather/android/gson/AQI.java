7f22c0a4774eabf000596f2b8ee346e6e40fe829
hackeryang <375446972@qq.com>
2018-01-12 15:48:03 +0800

    add in the function of display weather information.

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by root on 18-1-10.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
